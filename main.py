ab55feb7c55c1725305ce372c9e9f6482b9e827e
Trent Robinett <37916486+fireonehero@users.noreply.github.com>
2024-04-28 02:30:18 -0500

    Started main.py and made globals.py

---
+++ main.py
@@ -0,0 +1,36 @@
+import pygame
+import sys
+from globals import *
+
+class Game:
+    def __init__(self) -> None:
+        pygame.init()
+        self.screen = pygame.display.set_mode((SCREENWIDTH, SCREENHEIGHT))
+        self.clock = pygame.time.Clock()
+
+        self.running = True;
+    
+    def run(self):
+        while self.running:
+            self.update()
+            self.draw()
+        self.close()
+
+    def update(self):
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                self.running = False
+
+        pygame.display.update()
+        self.clock.tick(FPS)
+
+    def draw(self):
+        self.screen.fill('lightblue')
+
+    def close(self):
+        pygame.quit()
+        sys.exit()
+
+if __name__ == "__main__":
+    game = Game()
+    game.run()
