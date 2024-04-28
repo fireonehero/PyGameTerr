c9afcb3890d79af674f788d966beceb74ab88d6d
Trent Robinett <37916486+fireonehero@users.noreply.github.com>
2024-04-28 03:02:40 -0500

    Added basic sprites and scene loading.
    Added player, scene, and sprite.py. Started building the framework for player movement. Added scene management and sprite loading.

--- sprite.py
+++ sprite.py
@@ -1,7 +1,12 @@
 import pygame
-from pygame.sprite import _Group
 from globals import *
+from globals import TILESIZE
 
 class Entity(pygame.sprite.Sprite):
-    def __init__(self, groups) -> None:
+    def __init__(self, groups, image = pygame.Surface((TILESIZE, TILESIZE)), position = (0, 0)) -> None:
         super().__init__(groups)
+        self.image = image
+        self.rect = self.image.get_rect(topleft = position)
+
+    def update(self):
+        self.rect.x += 1
