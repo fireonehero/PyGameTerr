74f62d88fc0bb7c5a776c21173d23c342a8f04e3
Trent Robinett <37916486+fireonehero@users.noreply.github.com>
2024-04-28 02:38:02 -0500

    Added scene.py

---
+++ scene.py
@@ -0,0 +1,12 @@
+import pygame
+from globals import *
+
+class Scene:
+    def __init__(self) -> None:
+        self.app = app
+
+        def update(self):
+            pass
+
+        def draw(self):
+            self.app.screen.fill('red')
